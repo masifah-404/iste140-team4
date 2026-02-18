555282be38b4f5b2dfc5914e3f10977e48809e4d
Masifah <mm3726@g.rit.edu>
2026-02-18 20:54:26 +0400

    Add Spring Boot scaffold and static index page

---
+++ shatterRealms/src/main/java/org/example/shatterrealms/ShatterRealmsApplication.java
@@ -0,0 +1,13 @@
+package org.example.shatterrealms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShatterRealmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShatterRealmsApplication.class, args);
+    }
+
+}
---
+++ shatterRealms/src/test/java/org/example/shatterrealms/ShatterRealmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shatterrealms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShatterRealmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
