63dab4bb13711531483e7ed25a5d2aa21d7ce9bf
Masifah <mm3726@g.rit.edu>
2026-02-18 22:56:02 +0400

    Added second bean file

---
+++ shatterRealms/src/main/java/org/example/shatterrealms/models/Member.java
@@ -0,0 +1,43 @@
+package org.example.shatterrealms.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Member {
+    private Long memberId;
+    private String memberName;
+    private String memberEmail;
+    private String memberPassword;
+
+    public String getMemberPassword() {
+        return memberPassword;
+    }
+
+    public void setMemberPassword(String memberPassword) {
+        this.memberPassword = memberPassword;
+    }
+
+    public String getMemberEmail() {
+        return memberEmail;
+    }
+
+    public void setMemberEmail(String memberEmail) {
+        this.memberEmail = memberEmail;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+}
---
+++ shatterRealms/src/main/java/org/example/shatterrealms/models/MerchProduct.java
@@ -0,0 +1,54 @@
+package org.example.shatterrealms.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MerchProduct {
+
+    private Long productId;
+    private String productName;
+    private String productDescription;
+    private Double productPrice;
+    private int stock;
+
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
