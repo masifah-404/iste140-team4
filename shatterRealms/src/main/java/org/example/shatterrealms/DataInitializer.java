48e70c6ba20b546c82e5fc413a991fd908f1296f
Masifah <mm3726@g.rit.edu>
2026-04-26 23:14:43 +0400

    Add data seeding

---
+++ shatterRealms/src/main/java/org/example/shatterrealms/DataInitializer.java
@@ -0,0 +1,26 @@
+package org.example.shatterrealms;
+
+import org.example.shatterrealms.models.Member;
+import org.example.shatterrealms.repositories.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Autowired private MemberRepository memberRepository;
+
+    @Override
+    public void run(String... args) {
+        seedMembers();
+    }
+
+    private void seedMembers() {
+        if (memberRepository.count() > 0) return;
+        memberRepository.save(new Member("Masifah",  "masifahm@shatterrealms.com", "m@sif@h"));
+        memberRepository.save(new Member("Safia",    "safias@shatterrealms.com",   "s@fi@"));
+        memberRepository.save(new Member("Arina",    "arinab@shatterrealms.com",   "@rin@"));
+    }
+
+}
