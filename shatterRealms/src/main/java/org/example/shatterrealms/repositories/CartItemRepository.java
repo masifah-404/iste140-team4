ab523d45ac2b2eb8acb31a581ee089a66f256bbb
Arina <arinaaabayazitovaaa@gmail.com>
2026-04-26 23:53:03 +0400

    Updated CartItemRepository.java

--- shatterRealms/src/main/java/org/example/shatterrealms/repositories/CartItemRepository.java
+++ shatterRealms/src/main/java/org/example/shatterrealms/repositories/CartItemRepository.java
@@ -1,6 +1,6 @@
 package org.example.shatterrealms.repositories;
 
-import org.example.shatteredrealms.models.CartItem;
+import org.example.shatterrealms.models.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
