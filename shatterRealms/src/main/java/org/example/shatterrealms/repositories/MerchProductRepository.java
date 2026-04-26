6395f8c03996fe1e8d2c55b03ca0bc7436576f1e
SafiaRIT <ss6053@g.rit.edu>
2026-04-26 19:02:10 +0400

    added methods and removed redundanct annotation

--- shatterRealms/src/main/java/org/example/shatterrealms/repositories/MerchProductRepository.java
+++ shatterRealms/src/main/java/org/example/shatterrealms/repositories/MerchProductRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,7 +19,18 @@ public interface MerchProductRepository extends JpaRepository<MerchProduct, Long
 
     // Update method
     @Modifying
-    @Transactional
     @Query("UPDATE MerchProduct p SET p.stock = :stock WHERE p.productId = :id")
     int updateStockById(@Param("id") Long id, @Param("stock") int stock);
+
+    // Find method for exact name search
+    List<MerchProduct> findByProductName(String name);
+
+    // Find method for items in low stock
+    List<MerchProduct> findByStockLessThan(int limit);
+
+    // Find method for finding product in a price range
+    List<MerchProduct> findByProductPriceBetween(Double min, Double max);
+
+    // Find method for finding products and sorting them in descending order of price
+    List<MerchProduct> findAllByOrderByProductPriceDesc();
 }
